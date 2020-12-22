f42804545d261119b689924d7a25792269a24930
Lasse Skov <lasse.skov@gmail.com>
2020-12-22 07:41:25 +0100

    Created duplicate of csminify folder

---
+++ cmd/gi-parser/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	demoinfocs "github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs"
+	msgpack "gopkg.in/vmihailenco/msgpack.v2"
+
+	min "github.com/markus-wa/cs-demo-minifier"
+	pb "github.com/markus-wa/cs-demo-minifier/protobuf"
+	rep "github.com/markus-wa/cs-demo-minifier/replay"
+)
+
+func main() {
+	fl := new(flag.FlagSet)
+	fl.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage of csminify:")
+		fl.PrintDefaults()
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "May exit with code 3 if a demo ends unexpectedly, but the minified data may still be usable if this happens")
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "Direct bug reports and feature requests to https://github.com/markus-wa/cs-demo-minifier")
+	}
+
+	formatPtr := fl.String("format", "json", "Format into which the demo should me minified [json, msgpack, protobuf]")
+	freqPtr := fl.Float64("freq", 0.5, "Snapshot frequency - per second")
+	demPathPtr := fl.String("demo", "", "Demo file `path` (default stdin)")
+	outPathPtr := fl.String("out", "", "Output file `path` (default stdout)")
+
+	err := fl.Parse(os.Args[1:])
+	if err != nil {
+		// Some parsing problem, the flag.Parse() already prints the error to stderr
+		return
+	}
+
+	format := *formatPtr
+	freq := *freqPtr
+	demPath := *demPathPtr
+	outPath := *outPathPtr
+
+	err = minify(demPath, freq, format, outPath)
+	if err == demoinfocs.ErrUnexpectedEndOfDemo {
+		fmt.Fprintln(os.Stderr, "WARNING: encountered unexpected end of demo, but the minified data may still be usable")
+		os.Exit(3)
+	} else if err != nil {
+		panic(err)
+	}
+}
+
+func minify(demPath string, freq float64, format string, outPath string) error {
+	var marshaller min.ReplayMarshaller
+
+	switch format {
+	case "json":
+		marshaller = func(replay rep.Replay, w io.Writer) error {
+			return json.NewEncoder(w).Encode(replay)
+		}
+
+	case "protobuf":
+		fallthrough
+	case "proto":
+		fallthrough
+	case "pb":
+		marshaller = pb.MarshalReplay
+
+	case "msgpack":
+		fallthrough
+	case "mp":
+		marshaller = func(rep rep.Replay, w io.Writer) error {
+			return msgpack.NewEncoder(w).Encode(rep)
+		}
+
+	default:
+		fmt.Fprintf(os.Stderr, "Format '%s' unknown, known formats are 'json', 'msgpack' & 'protobuf'\n", format)
+		os.Exit(1)
+	}
+
+	var in io.Reader
+
+	switch demPath {
+	case "":
+		in = os.Stdin
+	default:
+		f, err := os.Open(demPath)
+		defer f.Close()
+		in = f
+
+		if err != nil {
+			return err
+		}
+	}
+
+	var out io.Writer
+
+	switch outPath {
+	case "":
+		out = os.Stdout
+	default:
+		f, err := os.Create(outPath)
+		defer f.Close()
+		out = f
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return min.MinifyTo(in, freq, marshaller, out)
+}
---
+++ cmd/gi-parser/main_test.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+var (
+	demPath = "../../test/cs-demos/default.dem"
+	outDir  = "../../test/results"
+	outPath = outDir + "/example"
+)
+
+func TestMain(m *testing.M) {
+	// Check if test demo exists
+	if _, err := os.Stat(demPath); err != nil {
+		panic(fmt.Sprintf("Can't read test demo %q: %s", demPath, err.Error()))
+	}
+	// Create test result folder if it doesn't exist, somehow 0666 isn't enough for travis??
+	if err := os.MkdirAll(outDir, 0777); err != nil {
+		panic(fmt.Sprintf("Couldn't create output dir %q: %s", outDir, err.Error()))
+	}
+	os.Exit(m.Run())
+}
+
+func TestUsage(t *testing.T) {
+	runMainWithArgs([]string{"-h"})
+}
+
+func TestStdInOut(t *testing.T) {
+	oldStdin := os.Stdin
+	defer func() { os.Stdin = oldStdin }()
+
+	f, err := os.Open(demPath)
+	os.Stdin = f
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out := outPath + ".stdout"
+	f, err = os.Create(out)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stdOut := os.Stdout
+	os.Stdout = f
+	runMainWithArgs([]string{})
+	os.Stdout = stdOut
+
+	assertOutFileCreated(out, t)
+}
+
+func TestInOut(t *testing.T) {
+	out := outPath + ".out"
+	runMainWithArgs([]string{"-demo", demPath, "-out", out})
+	assertOutFileCreated(out, t)
+}
+
+func TestFreq(t *testing.T) {
+	runMainWithArgs([]string{"-demo", demPath, "-freq", "0.2", "-out", os.TempDir() + "/demo-freq.out"})
+}
+
+func TestJSON(t *testing.T) {
+	testFormat("json", ".json", t)
+}
+
+func TestMsgPack(t *testing.T) {
+	testFormat("msgpack", ".mp", t)
+}
+
+func TestProtobuf(t *testing.T) {
+	testFormat("protobuf", ".pb", t)
+}
+
+func testFormat(format string, suffix string, t *testing.T) {
+	runMainWithArgs([]string{"-demo", demPath, "-format", format, "-out", outPath + suffix})
+	assertOutFileCreated(outPath+suffix, t)
+}
+
+func runMainWithArgs(args []string) {
+	// Store original arguments and restore them at the end
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+
+	// Add dummy command name to args
+	args = append([]string{"cmd"}, args...)
+	os.Args = args
+	main()
+}
+
+func assertOutFileCreated(path string, t *testing.T) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		t.Fatalf("Output file %q not created: %s", path, err.Error())
+	}
+}
